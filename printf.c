28607df9d7a71ecbfb825cc2514791c185722dbc
meranhany25 <meranhany25@gmail.c>
2023-09-17 20:49:21 -0700

    printf.c

---
+++ printf.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * _printf - printf format
+ * @format: funcion pointer
+ * Return: count
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+
+	int count = 0;
+
+	va_start(args, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+			case 'c':
+				count += print_char(args);
+				break;
+			case 's':
+				count += print_string(args);
+				break;
+			case '%':
+				putchar('%');
+				count++;
+				break;
+			case 'd':
+			case 'i':
+				count += print_decimal(args);
+				break;
+			default:
+				break;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
