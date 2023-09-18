bcaeb4e9a0907dfba0c8b447efae9af0757fe147
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-09-18 16:41:49 -0700

    integer.c

---
+++ integer.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * integer_print - handel charchter (%i)(%d)
+ * @list: arguments counter
+ * @count: pointer counts
+ * Return: count
+ */
+int integer_print(va_list list, int *count)
+{
+	int numbers = va_arg(list, int);
+
+	if (numbers < 0)
+	{
+		*count += _putchar('-');
+		numbers = -numbers;
+	}
+	*count += unsigned_print(numbers, 10, "0123456789");
+	printf("Enter");
+	return (*count);
+}
